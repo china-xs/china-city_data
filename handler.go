3d4f8d9fe780df38d50940542fd6650bbdcc6040
xs <xies.1@jifenn.com>
2021-11-18 11:46:17 +0800

    fix:修复东莞、中山、儋州市数据提取不完整问题

--- handler.go
+++ handler.go
@@ -17,6 +17,57 @@ import (
 	"time"
 )
 
+func handlerV2(url, pids, merger string, id, level int32) {
+	html := GetHtml(url)
+	dom, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		log.Fatal(err)
+	}
+	urlprix := url[:strings.LastIndex(url, "/")]
+	var code, name string
+	hasLink := false
+	var j = 1
+	dom.Find("a").Each(func(i int, selection *goquery.Selection) {
+		hasLink = true
+		t, _ := selection.Html()
+		if level == 1 {
+			name = t[:strings.Index(t, "<br/>")]
+			newId := save(id, level, name, pids, name, code, j)
+			j++
+			link, _ := selection.Attr("href")
+			ck := strconv.Itoa(int(newId))
+			cv := urlprix + "/" + link
+			Cache.Put(ck, cv, expire)
+		} else {
+			if i%2 == 0 {
+				code = t
+			} else {
+				name = t
+				newId := save(id, level, name, pids, merger+","+name, code, j)
+				link, _ := selection.Attr("href")
+				//cv := urlprix+"/"+link,
+				ck := strconv.Itoa(int(newId))
+				cv := urlprix + "/" + link
+				Cache.Put(ck, cv, expire)
+				j++
+			}
+		}
+
+	})
+	if !hasLink {
+		dom.Find(".villagetr").Each(func(i int, selection *goquery.Selection) {
+			t, _ := selection.Html()
+			s1 := strings.Index(t, ">")
+			s2 := strings.Index(t, "</td>")
+			s3 := strings.LastIndex(t, "<td>")
+			s4 := strings.LastIndex(t, "</td>")
+			code := t[s1+1 : s2]
+			name := t[s3+4 : s4]
+			save(id, level, name, pids, merger+","+name, code, i+1)
+		})
+	}
+}
+
 func handler(url, pIds, merger string, id, level int32) {
 	html := GetHtml(url)
 	dom, err := goquery.NewDocumentFromReader(strings.NewReader(html))
@@ -25,7 +76,9 @@ func handler(url, pIds, merger string, id, level int32) {
 	}
 	urlPrix := url[:strings.LastIndex(url, "/")]
 	var code, name string
+	hasLink := false
 	dom.Find("a").Each(func(i int, selection *goquery.Selection) {
+		hasLink = true
 		t, _ := selection.Html()
 		if i%2 == 0 {
 			code = t
@@ -57,7 +110,22 @@ func handler(url, pIds, merger string, id, level int32) {
 			}
 		}
 	})
+	if !hasLink {
+		dom.Find(".villagetr").Each(func(i int, selection *goquery.Selection) {
+			t, _ := selection.Html()
+			s1 := strings.Index(t, ">")
+			s2 := strings.Index(t, "</td>")
+			s3 := strings.LastIndex(t, "<td>")
+			s4 := strings.LastIndex(t, "</td>")
+			code := t[s1+1 : s2]
+			name := t[s3+4 : s4]
+			save(id, level, name, pIds, merger+","+name, code, i+1)
+		})
+	}
 }
+
+// url pids merger pid level
+//url, pIds, merger string, id, level int32
 
 //东莞、中山 居委会
 func handler1(url, pIds, merger string, id, level int32) {
@@ -82,6 +150,7 @@ func handler1(url, pIds, merger string, id, level int32) {
 func GetHtml(url string) (html string) {
 	if Cache.IsExist(url) {
 		c := Cache.Get(url)
+		Cache.Put(url, c, expire)
 		return c.(string)
 	}
 	s := rand.Intn(3)
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"gorm.io/gorm/schema"
 	"os"
 	"path"
+	"strconv"
 	"time"
 )
 
@@ -19,17 +20,82 @@ var (
 
 const (
 	host   = "192.168.56.12"
-	user   = "root"
-	pwd    = "root"
+	user   = "db_user"
+	pwd    = "db_pass"
 	port   = 3306
-	dbname = ""
+	dbname = "yjf_scrm_1000001"
 	preFix = "oa_"
+	table  = "m_district"
 )
 
 func main() {
 	//sk := Client("https://baidu.com")
 	//fmt.Printf("sk:%v,skType:%T\n ss:%v",sk,sk, sk=="")
-	GetSf(true, 1)
+	//GetSf(true, 1)
+	var results []MDistrict
+	var pids string
+	url := "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/index.html"
+	for i := 1; i <= 5; i++ {
+		if i > 1 {
+			//db.Table(table).Create(&save)
+			db.Table(table).Where("level = ?", i-1).FindInBatches(&results, 10, func(tx *gorm.DB, batch int) error {
+				for _, result := range results {
+					// 批量处理找到的记录
+					ck := strconv.Itoa(int(result.ID))
+					if Cache.IsExist(ck) {
+						c := Cache.Get(ck)
+						url = c.(string)
+						if result.PIds != "" {
+							pids = result.PIds + "," + ck
+						} else {
+							pids = ck
+						}
+						merger := result.Merger
+						handlerV2(url, pids, merger, result.ID, int32(i))
+					}
+				}
+				return nil
+			})
+		} else {
+			handlerV2(url, "", "", 0, 1)
+		}
+	}
+	/*
+		url := "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/44.html"
+		url ="http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2020/44/20/442000002.html"
+		html := GetHtml(url)
+		dom, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+		if err != nil {
+			log.Fatal(err)
+		}
+		//urlPrix := url[:strings.LastIndex(url, "/")]
+		//var code, name string
+		hasALink := false
+		dom.Find("a").Each(func(i int, selection *goquery.Selection) {
+			hasALink = true
+
+		})
+		if !hasALink {
+			html := GetHtml(url)
+			dom, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+			if err != nil {
+				log.Fatal(err)
+			}
+			//urlPrix := url[:strings.LastIndex(url,"/")]
+			dom.Find(".villagetr").Each(func(i int, selection *goquery.Selection) {
+				t, _ := selection.Html()
+				s1 := strings.Index(t, ">")
+				s2 := strings.Index(t, "</td>")
+				s3 := strings.LastIndex(t, "<td>")
+				s4 := strings.LastIndex(t, "</td>")
+				code := t[s1+1 : s2]
+				name := t[s3+4 : s4]
+				fmt.Printf("code:%v,name:%v\n", code,name)
+				//save(id, level, name, pIds, merger+","+name, code, i+1)
+			})
+		}
+
+	*/
 
 }
 
